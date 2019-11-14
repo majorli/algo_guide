f852aedea122a8061567ae0dfb42e944554f5a94
majorli <154788733@qq.com>
2019-11-14 23:44:43 +0800

    20191114_2344

---
+++ source/codes/213_palindrom.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int digit_at(int num, int d)
+{
+	while (--d) num /= 10;
+	return num % 10;
+}
+
+int reverse(int n)
+{
+	int r = n % 10;
+	while (n /= 10) r = 10 * r + n % 10;
+	return r;
+}
+
+bool is_palin_num(int n)
+{
+	return reverse(n) == n;
+}
+
+int main()
+{
+	using namespace std;
+
+	int num;
+	cin >> num;
+
+	for (int i = 1; i <= 10; i++)
+		cout << digit_at(num, i) << endl;
+	cout << "=================" << endl << reverse(num) << endl;
+	cout << "is " << (is_palin_num(num) ? "" : "not")
+		<< " a palindrom" << endl;
+
+	return 0;
+}
