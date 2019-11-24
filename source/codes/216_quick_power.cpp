c3a5d5b8fa051f6e754bf625cf8343ec6869fe7f
majorli <154788733@qq.com>
2019-11-24 11:28:53 +0800

    20191124_1128

---
+++ source/codes/216_quick_power.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+long long quick_power(int a, unsigned int n)
+{
+	long long ans = 1, base = a;
+	while (n) {
+		if (n & 1) ans *= base;
+		n >>= 1;
+		base *= base;
+	}
+	return ans;
+}
+
+int main()
+{
+	printf("0^0 = %lld\n", quick_power(0, 0));
+	printf("3^0 = %lld\n", quick_power(3, 0));
+	printf("3^13 = %lld\n", quick_power(3, 13));
+	printf("-3^13 = %lld\n", quick_power(-3, 13));
+	printf("-3^14 = %lld\n", quick_power(-3, 14));
+	printf("2^62 = %lld\n", quick_power(2, 62));
+	printf("2^63 = %lld\n", quick_power(2, 63));
+
+	return 0;
+}
