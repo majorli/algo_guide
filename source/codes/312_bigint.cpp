a1a53b3cb026730df0133c028501973caeefaba0
majorli <154788733@qq.com>
2019-12-24 00:14:36 +0800

    20191224_0014draft

---
+++ source/codes/312_bigint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct BigInt {
+	private:
+		static const int BASE = 10000;
+		static const int WIDTH = 4;
+        	vector<int> s;
+};
+
+int main()
+{
+	return 0;
+}
