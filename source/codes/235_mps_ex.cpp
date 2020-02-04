796ee258f54e1d842580fc583659a801b5919dcb
majorli <154788733@qq.com>
2020-02-05 01:37:47 +0800

    20200205_0137draft

--- source/codes/235_mps_ex.cpp
+++ source/codes/235_mps_ex.cpp
@@ -13,20 +13,41 @@ int main()
 	start = clock();
 
 	int sz = strlen(s);
+	// len1, len2: 以s[i]为中心和以s[i]s[i+1]中间的空隙为中心进行的两次中心扩展得到的回文子串长度
+	// len1最小为1，因为单个字符s[i]肯定是回文串
+	// len2最小为0，如果s[i] != s[i+1]，那么len2就等于0
+	// len：取len1和len2中的大者
+	// maxlen：保存下来的到目前为止找到的最长回文子串长度
+	// left：保存下来的到目前为止找到的最长回文子串的起始位置（左端点）
+	// maxlen初始为1，left初始为0，表示至少字符s[0]肯定是一个回文子串
 	int len1, len2, len, maxlen = 1, left = 0;
 	for (int i = 0; i < sz; ++i) {
-		len1 = expand(s, i, i);
-		len2 = expand(s, i, i+1);
-		len = len1 > len2 ? len1 : len2;
-		if (len > maxlen) {
+		len1 = expand(s, i, i);		// 以s[i]为中心进行扩展
+		len2 = expand(s, i, i+1);	// 以s[i]和s[i+1]中间的空隙为中心进行扩展
+		len = len1 > len2 ? len1 : len2;// 取两次扩展得到的回文子串长度中的大者
+		if (len > maxlen) {		// 若len大于已知的最长回文子串，则更新maxlen和left
 			maxlen = len;
+			// 假设len来自于len1，表示以s[i]为中心的长度为len的子串为
+			//     目前已知的最长回文子串，此时len必为奇数，最小值为1
+			//     设len = len1 = 2 * k + 1，(k=0,1,2,...)
+			//     那么此回文子串为s[i-k]...s[i]...s[i+k]
+			//     即第一个字符的位置为left = i - k = i - (len - 1) / 2
+			// 假设len来自于len2，表示以s[i]和s[i+1]为中心的长度为len的子串为
+			//     目前已知的最长回文子串，此时len必为偶数。由于len1至少为1，
+			//     而现在len2比len1更大，所以len最小值为2
+			//     设len = len2 = 2 * k，(k=1,2,...)
+			//     那么此回文子串为s[i-k+1]...s[i]s[i+1]...s[i+k]
+			//     即第一个字符的位置为i - k + 1 = i - (k - 1)
+			//     因为len为偶数，利用C++整数除法只取商的特性，有：
+			//     (len - 1) / 2 = (2*k - 1) / 2 = k - 1
+			//     所以这种情况下，同样有left = i - (len - 1) / 2
 			left = i - (len - 1) / 2;
 		}
 	}
 	
 	end = clock();
 
-	while (maxlen--) putchar(s[left++]);
+	while (maxlen--) putchar(s[left++]);	// 从s[left]开始连续输出maxlen个字符
 	putchar('\n');
 
 	printf("time elapsed = %ld ms.\n", (end - start) / (CLOCKS_PER_SEC / 1000));
