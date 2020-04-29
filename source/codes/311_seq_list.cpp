91255028406626a5eb6c04b979c03a4127c70e4b
majorli <154788733@qq.com>
2020-04-29 12:58:22 +0800

    20200429_1258draft_proofread_up_to_311

--- source/codes/311_seq_list.cpp
+++ source/codes/311_seq_list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 const int MAXN = 1e6;
 
@@ -7,40 +8,41 @@ struct List {
 	int size;
 
 	List() { size = 0; }
-
-	int insert(int val, int pos = -1)
-	{
-		if (size == MAXN)
-			return -1;
-		if (pos < 0 || pos > size)
-			pos = size;
-		for (int i = size - 1; i >= pos; i--)
-			data[i+1] = data[i];
-		data[pos] = val;
-		size++;
-		return pos;
-	}
-	
-	int remove(int pos = -1)
-	{
-		if (pos < 0 || pos >= size)
-			pos = size - 1;
-		int t = data[pos];
-		for (int i = pos + 1; i < size; i++)
-			data[i-1] = data[i];
-		size--;
-		return t;
-	}
-
-	int get(int pos)
-	{
-		if (pos < 0 || pos >= size)
-			throw "pos error";
-		return data[pos];
-	}
+	int insert(int val, int pos = -1);
+	int remove(int pos = -1);
+	int get(int pos) const;
 };
 
-using namespace std;
+int List::insert(int val, int pos)
+{
+	if (size == MAXN)
+		return -1;
+	if (pos < 0 || pos > size)
+		pos = size;
+	for (int i = size - 1; i >= pos; i--)
+		data[i+1] = data[i];
+	data[pos] = val;
+	size++;
+	return pos;
+}
+
+int List::remove(int pos)
+{
+	if (pos < 0 || pos >= size)
+		pos = size - 1;
+	int t = data[pos];
+	for (int i = pos + 1; i < size; i++)
+		data[i-1] = data[i];
+	size--;
+	return t;
+}
+
+int List::get(int pos) const
+{
+	if (pos < 0 || pos >= size)
+		throw "pos error";
+	return data[pos];
+}
 
 int main()
 {
