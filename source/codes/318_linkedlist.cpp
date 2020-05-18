85dbc91910ab60a334671cdbd396e9f07761c754
majorli <154788733@qq.com>
2020-05-18 12:11:02 +0800

    20200518_1210draft_318_writing

--- source/codes/318_linkedlist.cpp
+++ source/codes/318_linkedlist.cpp
@@ -13,21 +13,21 @@ struct LinkedList {
 	};
 
 	// 模仿STL容器的迭代器，包装节点指针为一个可迭代的类型
-	struct Index {
+	struct Indicator {
 		LinkedList<T>::_Node *_ptr;
 
-		// 构造器
-		Index(LinkedList<T>::_Node *p = NULL) { _ptr = p; }
+		// 构造函数
+		Indicator(LinkedList<T>::_Node *p = NULL) { _ptr = p; }
 		// 重载解指针运算'*'，直接返回节点中的元素值
 		T &operator*() { return _ptr->_value; }
 		// 重载++运算，实现向后迭代
-		Index &operator++() { _ptr = _ptr ? _ptr->_next : NULL; return *this; }
-		Index operator++(int) {
-			Index temp = *this;
+		Indicator &operator++() { _ptr = _ptr ? _ptr->_next : NULL; return *this; }
+		Indicator operator++(int) {
+			Indicator temp = *this;
 			_ptr = _ptr ? _ptr->_next : NULL;
 			return temp;
 		}
-		// 判断节点指针是否为空，用于判断迭代结束
+		// 判断节点指针是否为空，用于判断迭代是否结束
 		bool end() const { return _ptr == NULL; }
 	};
 
@@ -39,33 +39,20 @@ struct LinkedList {
 	// 基本功能
 	int size() const { return _size; }
 	bool empty() const { return _size == 0; }
-	void clear();
 	// 迭代访问
-	LinkedList<T>::Index begin() const { return LinkedList<T>::Index(_head->_next); }
+	LinkedList<T>::Indicator begin() const { return LinkedList<T>::Indicator(_head->_next); }
 	// 元素访问
 	T &operator[](int i);
 	T &front() { return _head->_next->_value; }
 	// 增加删除
 	void push_front(const T &val);
-	void insert(const T &val, const LinkedList<T>::Index prev);
+	void insert(const T &val, const LinkedList<T>::Indicator prev);
 	void pop_front();
-	void erase(LinkedList<T>::Index prev);
+	void erase(LinkedList<T>::Indicator prev);
+	void clear();
 	// 析构函数
 	~LinkedList();
 };
-
-template<typename T>
-void LinkedList<T>::clear()
-{
-	LinkedList<T>::_Node *p = _head->_next, *next;
-	while (p) {
-		next = p->_next;
-		delete p;
-		p = next;
-	}
-	_size = 0;
-	_head->_next = NULL;
-}
 
 template<typename T>
 T &LinkedList<T>::operator[](int i)
@@ -85,7 +72,7 @@ void LinkedList<T>::push_front(const T &val)
 }
 
 template<typename T>
-void LinkedList<T>::insert(const T &val, const LinkedList<T>::Index prev)
+void LinkedList<T>::insert(const T &val, const LinkedList<T>::Indicator prev)
 {
 	LinkedList<T>::_Node *new_node = new LinkedList<T>::_Node(val);
 	new_node->_next = prev._ptr->_next;
@@ -104,12 +91,25 @@ void LinkedList<T>::pop_front()
 }
 
 template<typename T>
-void LinkedList<T>::erase(LinkedList<T>::Index prev)
+void LinkedList<T>::erase(LinkedList<T>::Indicator prev)
 {
 	LinkedList<T>::_Node *node = prev._ptr->_next;
 	prev._ptr->_next = node->_next;
 	delete node;
 	--_size;
+}
+
+template<typename T>
+void LinkedList<T>::clear()
+{
+	LinkedList<T>::_Node *p = _head->_next, *next;
+	while (p) {
+		next = p->_next;
+		delete p;
+		p = next;
+	}
+	_size = 0;
+	_head->_next = NULL;
 }
 
 template<typename T>
@@ -135,7 +135,7 @@ int main()
 	LinkedList<Point> lst;
 	printf("lst.size() = %d\n", lst.size());
 	printf("lst.empty() = %s\n", lst.empty() ? "true" : "false");
-	LinkedList<Point>::Index ptr = lst.begin();
+	LinkedList<Point>::Indicator ptr = lst.begin();
 	while (!ptr.end()) (*ptr++).show();
 
 	lst.push_front(Point(1.0, 2.0));
